84e9d7524de8ae580667766849b1fa595e605ed8
Patrick <pamarx92@gmail.com>
2022-10-06 20:35:59 +0200

    startet to add localStorage

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import './App.css';
 import Header from "../src/components/header/Header"
 import Footer from "../src/components/footer/Footer"
 import Cards from "../src/pages/Cards"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import cards from './assets/Db'
 import Bookmarks from './pages/Bookmarks'
 import Create from "../src/pages/Create"
 import Profile from "../src/components/profile/Profile"
 import { Route, Routes } from 'react-router-dom';
+import {setLocalStorage, loadLocalStorage} from './assets/Storage';
 
-
-
-function App(bookmarked) {
-  const [allCards, setAllCards] = useState(cards);
+function App() {
+  const [allCards, setAllCards] = useState(loadLocalStorage("allCards") ?? cards);
   const [state, isActive] = useState("home");
 
+useEffect(() => {
+  setLocalStorage("localSavedCards", allCards);
+}, [allCards])
 
   const bookmark = (id) => {
-    setAllCards(allCards.map((card) => card.id === id ? { ...card, bookmarked: !bookmarked } : card))
+    setAllCards(allCards.map((card) => card.id === id ? { ...card, bookmarked: !card.bookmarked } : card))
   }
 
   return (
--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,18 +1,21 @@
 import "../cards/Card.css"
 import { BsBookmarkStarFill } from 'react-icons/bs';
 import { useState } from "react"
+import cards from '../../assets/Db'
 
-
-function Card({ question, answer, tags }) {
+function Card({ question, answer, tags, bookmark}) {
     const [state, setState] = useState(false);
-    const [color, setColor] = useState('white');
+    const [bookmarkColor, setBookmarkColor] = useState(cards.bookmarked);
 
+    const handleBookmark = (id) => {
+        setBookmarkColor(cards.map((card) => card.id === id ? { ...card, bookmarked: !card.bookmark } : card))
+    }
 
     return (
         <>
             <div className="card">
-                <button className="card--icon">
-                    <BsBookmarkStarFill style={{ color: { color }, fontSize: '50px' }} />
+                <button className="card--icon" onClick={handleBookmark}>
+                    <BsBookmarkStarFill style={{ color:  bookmarkColor ? "white" : "black" , fontSize: '50px' }} />
                 </button>
                 <h2 className="card-question--headline">{question}</h2>
 
