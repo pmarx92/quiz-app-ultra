55a838ac094e1c922145fe02fb095598d0ac7fc8
Patrick <pamarx92@gmail.com>
2022-10-15 17:45:19 +0200

    removed code

--- src/App.js
+++ src/App.js
@@ -13,12 +13,11 @@ import { useState, useEffect } from "react"
 function App() {
   const [allCards, setAllCards] = useState(loadLocalStorage("allCards") ?? cardArray);
   const [showActivePage, setshowActivePage] = useState("home");
-  const [bookmarkedCards, setBookmarkedCards] = useState(loadLocalStorage("bookmarked") ?? []);
+
 
 
   useEffect(() => {
     setLocalStorage("localSavedCards", allCards);
-    setLocalStorage("bookmarked", bookmarkedCards)
   }, [allCards])
 
   const handleBookmark = (id) => {
